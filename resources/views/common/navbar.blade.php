219780a53c317af44b8c5fa62e8a77e0ad42bebe
Quan Do <quan.dm@teko.vn>
2017-12-20 07:47:24 +0000

    update

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -16,7 +16,7 @@
                 <li class="has-submenu">
                     <a href="#"><i class="md md-class"></i>Hệ thống</a>
                     <ul class="submenu">
-                        <li><a href="{{ url('/users')}}">User</a></li>
+                        <li><a href="{{ url('/admin/users')}}">User</a></li>
                     </ul>
                 </li>
                 @endif
